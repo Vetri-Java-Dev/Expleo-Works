1c77d59fba3250832fd6df26f8ed228ef27eee88
Vetri-Java-Dev <2k22cse161@kiot.ac.in>
2026-03-09 16:22:30 +0530

    Second commit

---
+++ src/demo/JenkinsDemo.java
@@ -0,0 +1,18 @@
+package demo;
+import java.util.Scanner;
+
+public class JenkinsDemo {
+	public static void main(String[] args) {
+		
+		Scanner scanner=new Scanner(System.in);
+		
+		System.out.print("Enter a value : ");
+		int a=scanner.nextInt();
+		
+		System.out.print("Enter b value : ");
+		int b=scanner.nextInt();
+		
+		System.out.println("Addition : "+(a+b));
+		
+	}
+}
